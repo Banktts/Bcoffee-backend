b278e9ef44c12ce9a7f0ad9cdb1f57c98f0976d7
Palita <amppalita0905@gmail.com>
2020-11-19 17:39:55 +0700

    add orderline

--- routes/order.js
+++ routes/order.js
@@ -61,7 +61,90 @@ Router.post("/post/order", (req, res) => {
   });
 
 
+  Router.post("/post/orderline", (req, res) => {
+    let order_id = req.body.orderId;
+    let menu_id = req.body.menuId;
+    let amount = req.body.amount;
+    const sql =
+      "SET @order_id = ?;SET @menu_id = ?;SET @amount = ?;CALL placeOrderline(@order_id, @menu_id, @amount)";
+    mysqlConnection.query(
+      sql,
+      [
+        order_id,
+        menu_id,
+        amount
+      ],
+      (err, results, fields) => {
+        if (!err) {
+          results.forEach((element) => {
+            if (element.constructor == Array) res.send(element);
+          });
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
 
+
+  Router.get("/orderline/:orderId/:menuId", (req, res) => {
+    let order_id = req.params.orderId;
+    let menu_id = req.params.menuId;
+    mysqlConnection.query(
+      `select  M.name, amount, O.amount*M.price as price 
+      from orderline O, menu M 
+      where O.menu_id = M.menu_id and O.order_id = ${order_id} and O.menu_id = ${menu_id};`,
+      (err, results, fields) => {
+        if (!err) {
+          res.send(results);
+        } else {
+          console.log(err);
+        }
+      }
+    );
+   });
+
+
+   Router.put("/update/orderline/:orderId/:menuId", (req, res) => {
+    let order_id = req.params.orderId
+    let menu_id = req.params.menuId
+    let newAmount = req.body.amount;
+    const sql =
+      "SET @order_id = ?;SET @menu_id = ?;SET @newAmount = ?;CALL updateAmountOrderline(@order_id, @menu_id, @newAmount)";
+    mysqlConnection.query(
+      sql,
+      [
+        order_id,
+        menu_id,
+        newAmount
+      ],
+      (err, results, fields) => {
+        if (!err) {
+          res.send(
+            "The data for the selected quarterback has been successfully updated."
+          );
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
+
+
+  Router.delete("/delete/orderline/:orderId/:menuId", (req, res) => {
+    let order_id = req.params.orderId
+    let menu_id = req.params.menuId
+    mysqlConnection.query(
+      `DELETE FROM orderline WHERE order_id= ${order_id} and menu_id= ${menu_id} `,
+      (err, results, fields) => {
+        if (!err) {
+          res.send("The selected quarterback has been successfully deleted.");
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
 
 
   //6--------------------------------------------------------------------------รอภีมมมมมมม
